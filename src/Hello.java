f3de40c07c3317d7dab4ec3b63c40fd90b4789bb
vaibhus23 <vaibhus23@gmail.com>
2019-03-30 14:51:00 +0530

    added my first commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,8 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("my first commit");
 
 	}
 
